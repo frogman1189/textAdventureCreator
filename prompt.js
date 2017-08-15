e25e7b4dd4fb3ff8defc99452276a5339c6f5de9
Logan Warner <frogman1189@gmail.com>
2017-08-16 09:20:18 +1200

    Interacts with the prompt now. Still need to add in items and objects and events

--- prompt.js
+++ prompt.js
@@ -2,6 +2,9 @@ var global_currentArea = null;
 var global_startArea = null;
 var global_areaArray = [];
 
+
+
+
 //Class Area, used to manage each area in the text adventure
 class Area {
 	// contruct area -- declare all variables
@@ -25,16 +28,32 @@ class Area {
 		this.destinations[direction] = areaIndex;
 	}
 	// move in direction, if that direction points to an area then set global_currentArea to that area.
-	moveDestination(direction) {
-		var x = this.destinations[direction]
-		alert(direction, x);
-		if (x >= 0) {
-			global_currentArea = global_areaArray[x];
+	move(dir) {
+		//console.log(this.destinations[dir]);
+		if (this.destinations[dir] != undefined) {
+			global_currentArea = global_areaArray[this.destinations[dir]];
+			display(global_currentArea.description);
+			//console.log(global_currentArea.description);
 		}
 	}
 }
 
-
+function runMove(){
+	move();
+}
+function move() {
+		console.log("started");
+		input = document.getElementById("input");
+		input = input.value;
+		console.log(input);
+		input = input.split(" ");
+		/*for (i=0;i<input.length;i++) {
+			if(input[i] == "MOVE") {
+				global_currentArea.move(input[i+1]);
+			}
+		}*/
+		global_currentArea.move(input[1]);
+}
 
 // Destination data type -- not currently in use but probably will be later
 var Destination = function(direction, area) {
@@ -45,7 +64,7 @@ var Destination = function(direction, area) {
 
 //load Project
 function loadProject(file) {
-	file = file.split("\n");
+	file = file.split("\r\n\r\n");
 	for (var i=0; i < file.length; i++) {
 		global_areaArray[i] = JSON.parse(file[i]);
 		global_areaArray[i].__proto__ = Area.prototype;
@@ -58,7 +77,10 @@ function loadProject(file) {
 	}
 }
 
-
+function receiveText(e) {
+	file = e.target.result;
+	loadProject(file);
+}
 
 //load a local project/game/save
 function loadLocalProjectFile() {
@@ -72,14 +94,9 @@ function loadLocalProjectFile() {
 	}
 	else {
 		var fileReader = new window.FileReader();
-		fileReader.onload = recieveText; 
+		fileReader.onload = receiveText; 
 		file = fileReader.readAsText(input.files[0]);
 	}
-}
-
-function recieveText(e) {
-	file = e.target.result;
-	loadProject(file);
 }
 
 
@@ -88,7 +105,10 @@ function recieveText(e) {
 function saveProject() {
 	var outfile = "";
 	for (i in global_areaArray) {
-		outfile = outfile + JSON.stringify(global_areaArray[i]) + "\n";
+		outfile = outfile + JSON.stringify(global_areaArray[i]);
+		if(i != global_areaArray.length - 1) {
+			outfile = outfile + "\r\n\r\n";
+		}
 	}
 	save("project.json", outfile);
 }
@@ -106,31 +126,24 @@ function save(filename, data) {
 
 //attach event listener to the input checking whether [ENTER] is pressed, and if so running report() which processes the data
 function setInputListener() {
-	document.getElementById('input').addEventListener('keyup', function (e) {
+	document.getElementById('input').addEventListener('keydown', function (e) {
+		
 		var key = e.which || e.keyCode; 
 		if (key == 13) { 
+			e.preventDefault();
 			takeInput();
+			
 		} 
 	});
 }
 
 
-//Deal with input from prompt
-
-function takeInput() {
-	var input = document.getElementById("input"); //get input element
-	var history = document.getElementById("history"); //get history elemnt for appending previous commands
-	
-	var node = document.createElement("p"); //create new element which will be appended
-	node.setAttribute("class", "history"); //set the history paragraph class to 'history'
-	history.append(node, input.value); //append history paragraph and inpute value to histry div
-	resetPrompt();
-}
-
-
-//deal with the input
-function dealInput(input) {
-	
+function display(text) {
+	var history = document.getElementById("history");
+	var node = document.createElement("p");
+	node.setAttribute("class", "history");
+	history.append(node, text);
+	return 0;
 }
 
 
@@ -138,12 +151,9 @@ function dealInput(input) {
 
 function resetPrompt() {
 	input = document.getElementById("input");
-	input.value = ">>> ";
+	input.value = ">";
 	input.focus();
 }
-
-
-
 
 //function sets the scroll of the console back to the bottom (where the input is)
 
@@ -151,4 +161,39 @@ function updateScroll() {
 	var console = document.getElementById("console");
 	console.scrollTop = console.scrollHeight - console.clientHeight;
 }
+
+
+//Deal with input from prompt
+
+function takeInput() {
+	var input = document.getElementById("input"); //get input element
+	display(input.value);
+	/*var history = document.getElementById("history"); //get history elemnt for appending previous commands
+	
+	var node = document.createElement("p"); //create new element which will be appended
+	node.setAttribute("class", "history"); //set the history paragraph class to 'history'
+	history.append(node, input.value); //append history paragraph and inpute value to histry div*/
+	dealInput(input.value);
+	//move();
+	resetPrompt();
+}
+
+
+//deal with the input
+function dealInput(input) {
+	input = input.toUpperCase();
+	input = input.replace(/>/g, "");	/// this is the problem
+	console.log(input);
+	input = input.split(" ");
+	//console.log(global_currentArea.destinations[input[0]]);
+	for (i=0;i<input.length;i++) {
+		//console.log(input[i]);
+		if(input[i] == "MOVE") {
+			global_currentArea.move(input[i+1]);
+			return 0;
+		}
+
+	}
+}
+
 
